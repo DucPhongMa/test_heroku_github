ed7e679fbf7424e2033806511e18e86bcb47aa31
maducphong7@gmail.com <maducphong1998>
2022-05-13 14:35:56 -0400

    add comments in server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: _____Duc Phong Ma____Student ID: __137015194__ Date: __May 13, 2022_
+*
+*  Online (Heroku) URL: https://nameless-headland-14758.herokuapp.com/
+*
+********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
